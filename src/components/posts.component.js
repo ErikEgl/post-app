77930d3c1176ea1fb0c326ec267889bc64252b68
Erik Egliens <erik@bellcom.ee>
2021-12-25 00:55:22 +0200

    added loader component

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -1,14 +1,46 @@
-import { Component } from '../core/component'
-import { apiService } from '../services/api.service'
-import { TransformService } from '../services/transform.service'
+import { Component } from "../core/component";
+import { apiService } from "../services/api.service";
+import { TransformService } from "../services/transform.service";
 
 export class PostsComponent extends Component {
-  constructor(id) {
-    super(id)
+  constructor(id, {loader}) {
+    super(id);
+    this.loader = loader
   }
   async onShow() {
-    const fbData = await apiService.fetchPosts()
-    const posts = TransformService.fbObjectToArray(fbData)
-    console.log(posts);
+    this.loader.show()
+    const fbData = await apiService.fetchPosts();
+    const posts = TransformService.fbObjectToArray(fbData);
+    const html = posts.map(post => renderPost(post)) 
+    this.loader.hide()
+    this.$el.insertAdjacentHTML('afterbegin', html.join(' '))
   }
+  onHide() {
+    this.$el.innerHTML = ''
+  }
+}
+
+function renderPost(post) {
+  const tag = post.type === 'news' 
+    ? '<li class="tag tag-blue tag-rounded">Новость</li>'
+    : '<li class="tag tag-rounded">Заметка</li>'
+
+    const button = '<button class="button-small button-round button-primary">Сохранить</button>'
+  return `    
+    <div class="panel">
+      <div class="panel-head">
+        <p class="panel-title">${post.title}</p>
+        <ul class="tags">
+          ${tag}
+        </ul>
+      </div>
+      <div class="panel-body">
+        <p class="multi-line">${post.fulltext}</p>
+      </div>
+      <div class="panel-footer w-panel-footer">
+        <small>${post.date}</small>
+        ${button}
+      </div>
+    </div>
+  `;
 }
